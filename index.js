d45a8c1e7b850f9006e6b9af7d4ad40424843582
SuperstitiousSeiya <reyver.abellanosa@student.passerellesnumeriques.org>
2023-09-20 18:43:25 +0800

    added new line in js

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+
+
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+
+
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
+console.log("asdlkasjdlksadjklasdasdjalksdjalskdjksaldj")
